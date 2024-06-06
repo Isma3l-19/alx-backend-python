2e43f27d7ca800aeac322fba8d5e498328bf9882
Ismael Nyambu <133232846+Isma3l-19@users.noreply.github.com>
2024-06-06 15:22:14 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Returns a  floor float
+"""
+def floor(n: float) -> float:
+    """
+     a type-annotated function floor which takes a float n as argument and 
+     returns the floor of the float.
+    """
+    return floor(n)
